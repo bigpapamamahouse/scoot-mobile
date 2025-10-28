b7a5dfb77573e5611f50505fcb0557bf6bc08a90
bigpapamamahouse <grantswingler@gmail.com>
2025-10-27 20:34:49 -0500

    Delete media.ts

--- media.ts
+++
@@ -1,7 +0,0 @@
-
-import { ENV } from './env';
-export function mediaUrlFromKey(key?: string | null){
-  if(!key) return null;
-  const base = ENV.MEDIA_BASE.replace(/^https?:\/\//, '');
-  return `https://${base}/${encodeURIComponent(key)}`;
-}
