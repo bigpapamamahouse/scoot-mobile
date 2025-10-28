d8939db3aa9f6bf32304b80ef51f95358ca054a0
bigpapamamahouse <grantswingler@gmail.com>
2025-10-27 20:07:09 -0500

    Add files via upload

---
+++ awsConfig.ts
@@ -0,0 +1,23 @@
+// src/awsConfig.ts
+import { Amplify } from 'aws-amplify';
+import { loadGetRandomValues } from '@aws-amplify/react-native';
+import 'react-native-url-polyfill/auto';
+import { ENV } from './lib/env';
+
+loadGetRandomValues();
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: ENV.USER_POOL_ID,
+      userPoolClientId: ENV.USER_POOL_CLIENT_ID,
+      region: ENV.REGION,
+      // tell Amplify which credential types are allowed for sign in
+      loginWith: {
+        username: true,
+        email: true,
+        phone: false,
+      },
+    },
+  },
+});
