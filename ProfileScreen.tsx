f90e658b97f6d468b2bb6a9357a97c384d364d2d
bigpapamamahouse <grantswingler@gmail.com>
2025-10-27 20:32:48 -0500

    Delete ProfileScreen.tsx

--- ProfileScreen.tsx
+++
@@ -1,4 +0,0 @@
-
-import React from 'react';
-import { View, Text } from 'react-native';
-export default function ProfileScreen(){ return <View style={{ padding: 16 }}><Text>Profile</Text></View>; }
