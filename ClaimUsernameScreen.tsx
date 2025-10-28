3361cb8cf100eeb3eaafdcb66f71f9cdaf7f5977
bigpapamamahouse <grantswingler@gmail.com>
2025-10-27 20:31:19 -0500

    Delete ClaimUsernameScreen.tsx

--- ClaimUsernameScreen.tsx
+++
@@ -1,4 +0,0 @@
-
-import React from 'react';
-import { View, Text } from 'react-native';
-export default function ClaimUsernameScreen(){ return <View style={{ padding: 16 }}><Text>Claim Username (stub)</Text></View>; }
